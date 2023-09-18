514a7fb8a2acbc4577fa2c0a57df00360d2218f6
Nika Oniani <nikaoniani2018@gmail.com>
2023-09-18 21:54:17 +0400

    added a background with stars

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<p>hello</p>, document.getElementById("root"))
+import "./index.css";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
