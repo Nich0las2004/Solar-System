99dee9227ce1fd1f124947e133ffce3ad116eea8
Nika Oniani <nikaoniani2018@gmail.com>
2023-09-18 22:45:38 +0400

    a user can see different parts of sun, zoom in and zoom out

--- src/components/Scene/Scene.js
+++ src/components/Scene/Scene.js
@@ -1,6 +1,8 @@
 import { Canvas } from "@react-three/fiber";
 import { Stars } from "@react-three/drei";
 
+import { OrbitControls } from "@react-three/drei";
+
 import classes from "./Scene.module.css";
 
 import Sun from "../Sun/Sun";
@@ -9,10 +11,11 @@ const Scene = () => {
   return (
     <Canvas className={classes.canvas}>
       <Sun />
-
-      {/* displaying stars  */}
-
       <Stars />
+      <OrbitControls
+        minDistance={2}
+        maxDistance={5}
+      />
     </Canvas>
   );
 };
