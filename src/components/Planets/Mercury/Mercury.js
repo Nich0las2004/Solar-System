984749699ac9eff8cc70da903f2f54708774cdd4
Nika Oniani <nikaoniani2018@gmail.com>
2023-09-22 19:59:32 +0400

    mercury rotates around the sun

--- src/components/Planets/Mercury/Mercury.js
+++ src/components/Planets/Mercury/Mercury.js
@@ -1,16 +1,27 @@
 import { useRef } from "react";
 import mercuryTexture from "../../../assets/mercury-texture.jpg";
 
-import { TextureLoader } from "three";
+import { TextureLoader, Clock } from "three";
 import { useFrame } from "@react-three/fiber";
 
 const Mercury = () => {
   const mercuryRef = useRef();
 
+  const clock = new Clock();
+
   const texture = new TextureLoader().load(mercuryTexture);
 
   useFrame(() => {
     if (mercuryRef.current) {
+      const elapsedTime = clock.getElapsedTime();
+
+      // Calculate the new position along the circular path
+      const radius = 10; // Radius of the circular path
+      const x = radius * Math.cos(elapsedTime);
+      const z = radius * Math.sin(elapsedTime);
+
+      mercuryRef.current.position.set(x, 0, z);
+
       mercuryRef.current.rotation.y += 0.02;
     }
   });
